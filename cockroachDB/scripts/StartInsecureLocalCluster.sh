42fd0d521eb0aa95841c808795a9ec59e542a5e0
Joseph Scheuhammer <clown@alum.mit.edu>
2019-11-28 16:41:40 -0500

    NOJIRA: Moved database/user SQL commands into script.

--- cockroachDB/scripts/StartInsecureLocalCluster.sh
+++ cockroachDB/scripts/StartInsecureLocalCluster.sh
@@ -8,7 +8,8 @@
 # You may obtain a copy of the License at
 # https://github.com/GPII/universal/blob/master/LICENSE.txt
 
-# Starts up a local insecure database: a cluster of three nodes
+# Starts up a local insecure cockroach database: a cluster of three nodes
+# Creates a 'evaluate_cockroachdb' database and a 'maxroach' user
 
 cockroach start --insecure --store=node1 --listen-addr=localhost:26257 --http-addr=localhost:8080 --join=localhost:26257,localhost:26258,localhost:26259 --background
 sleep 2
@@ -17,3 +18,11 @@ sleep 2
 cockroach start --insecure --store=node3 --listen-addr=localhost:26259 --http-addr=localhost:8082 --join=localhost:26257,localhost:26258,localhost:26259 --background
 sleep 2
 cockroach init --insecure --host=localhost:26257
+
+cockroach sql --insecure -e "
+CREATE USER IF NOT EXISTS maxroach;
+CREATE DATABASE evaluate_cockroachdb;
+GRANT ALL ON DATABASE evaluate_cockroachdb TO maxroach;
+"
+
+echo "'evaluate_cockroachdb' initialized (empty)"
